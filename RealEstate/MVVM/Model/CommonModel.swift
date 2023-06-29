f9e9ade1244136c2060b737034494fcef679218b
reyhanRoshan1 <reyhanroshan241@gmail.com>
2023-06-29 21:54:02 +0530

    added model and Viewmodel common

---
+++ RealEstate/MVVM/Model/CommonModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommonModel.swift
+//  RealEstate
+//
+//  Created by Surinder kumar on 29/06/23.
+//
+
+import Foundation
---
+++ RealEstate/MVVM/ViewModel/UserViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserViewModel.swift
+//  RealEstate
+//
+//  Created by Surinder kumar on 29/06/23.
+//
+
+import UIKit
+
+class UserViewModel: NSObject {
+
+}
